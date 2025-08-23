0fd6b88900fc47bde8466c53025b8e42ce620600
MrPoWasTaken <jerrygw2004@gmail.com>
2025-08-22 23:50:26 -0400

    Added not working bullets so far

--- Scenes/poopGun/poop_gun.gd
+++ Scenes/poopGun/poop_gun.gd
@@ -1,4 +1,12 @@
 extends Node2D
+@onready var poopBullet = preload("res://Scenes/poopBullet/poopBullet.tscn") #@ symbol modifies onready to ensure it is loaded
+var bullet #stores the temp bullet object
 
-func _physics_process(delta: float) -> void: #the underscore is like private
-	position.x += 1
+func shoot():
+	if Input.is_action_just_pressed('shoot'):
+		bullet = poopBullet.instantiate() # just assignment 
+		add_child(bullet) #adds the bullet as a child  
+		#THIS DOESNT WORK YET BC IT IS ATTACHED TO PLAYER AND NOT WORLD
+	
+func _physics_process(delta: float) -> void:
+	shoot()
