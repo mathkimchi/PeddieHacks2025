39b4740c0565c05447259a91a159bdffbb62a634
mathkimchi <kingalvinjkim@gmail.com>
2025-08-23 00:54:12 -0400

    bullet as area and shoot bullet in correct direction

--- Scenes/poopBullet/poop_bullet.gd
+++ Scenes/poopBullet/poop_bullet.gd
@@ -1,4 +1,24 @@
-extends Node2D
+extends Area2D
 
-func _physics_process(delta: float) -> void: #the underscore is like private
-	position.x += 1
+@export
+var SPEED = 300
+@export
+var RANGE = 2000
+
+var traveled_distance = 0
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector2.RIGHT.rotated(self.global_rotation)
+	position += delta * SPEED * direction
+	
+	traveled_distance += delta * SPEED
+	if traveled_distance > RANGE:
+		queue_free() # self destruct
+
+
+func _on_body_entered(body: Node2D) -> void:
+	# TODO: damage enemy
+	# if body.is_in_group("Enemies"):
+	# 	body.take_damage(25)
+	# 	self.queue_free() # self destruct
+	pass
--- Scenes/poopGun/poop_gun.gd
+++ Scenes/poopGun/poop_gun.gd
@@ -1,13 +1,17 @@
 extends Node2D
-@onready var poopBullet = preload("res://Scenes/poopBullet/poopBullet.tscn") #@ symbol modifies onready to ensure it is loaded
-var bullet #stores the temp bullet object
+@onready var poopBullet = preload("res://Scenes/poopBullet/poopBullet.tscn") # @ symbol modifies onready to ensure it is loaded
+var bullet # stores the temp bullet object
 
-func shoot():
-	if Input.is_action_just_pressed('shoot'):
-		bullet = poopBullet.instantiate() # just assignment
-		# add the bullet under the world instead of player to not be relative to player position
-		get_node("/root/World").add_child(bullet)
-		bullet.global_position = self.global_position
+func shoot(event):
+	bullet = poopBullet.instantiate() # just assignment
+	# add the bullet under the world instead of player to not be relative to player position
+	get_node("/root/World").add_child(bullet)
+	
+	# make bullet shoot from the gun location and in the direction of mouse point from player
+	bullet.global_position = self.global_position
+	bullet.global_rotation = self.get_global_transform_with_canvas().get_origin().direction_to(event.position).angle()
+	
 
-func _physics_process(delta: float) -> void:
-	shoot()
+func _input(event) -> void:
+	if event.is_action_pressed("shoot"):
+		shoot(event)
